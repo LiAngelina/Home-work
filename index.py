fcae92e63d28f6501ec87317026751f83238b388
Ли Ангелина Витальевна <171016374@step.kz>
2025-02-08 08:56:38 +0500

    Second connit

---
+++ index.py
@@ -0,0 +1,25 @@
+# Задание 1
+# nums = [1,2,3,4,5,6,7,8,9,10]
+# sum = 0
+# for num in nums:
+#     sum += num
+# print(sum)
+
+# Задание 2
+# nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+# for num in nums:
+#     if num % 2 == 0:
+#         print(num, end=" ")
+
+# Задание 3
+# nums = [36,96,24,75,11,9,73,111,94]
+# print(max(nums))
+
+# Задание 4
+# for i in range(10,0,-1):
+#     print(i, end=" ")
+
+# Задание 5
+# for i in range(1,11):
+#     print(5*i)
+
